95762377d8a0cc8f9c3d7f83c3a0ed343edce37b
unknown <Monirul-Jim>
2023-05-18 14:25:33 +0600

    gallery section database add

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../pages/Bannar/Banner';
 import Gallery from '../pages/Gallery/Gallery';
+import ReactTab from '../pages/Tab/ReactTab';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <Gallery></Gallery>
+           <ReactTab></ReactTab>
         </div>
     );
 };
--- src/component/pages/Gallery/Gallery.jsx
+++ src/component/pages/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import GalleryCard from './GalleryCard/GalleryCard';
 const Gallery = () => {
     const [gallery,setGallery]=useState([])
     useEffect(()=>{
-        fetch('http://localhost:5000/section')
+        fetch('http://localhost:5000/gallery-photo')
         .then(res=>res.json())
         .then(data=>setGallery(data))
     },[])
@@ -13,7 +13,7 @@ const Gallery = () => {
             <h1 className='mt-8 text-3xl font-semibold text-center'>All the product are available in our shop</h1>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8'>
                 {
-                    gallery.map(data=><GalleryCard key={data.id} data={data} ></GalleryCard>)
+                    gallery.map(data=><GalleryCard key={data._id} data={data} ></GalleryCard>)
                 }
             </div>
         </div>
