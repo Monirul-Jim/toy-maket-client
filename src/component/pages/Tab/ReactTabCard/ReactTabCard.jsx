ec07669f27e22a95483c0f6f2775c561e272121b
unknown <Monirul-Jim>
2023-05-19 16:44:12 +0600

    click on button show a alert

--- src/component/pages/Tab/ReactTabCard/ReactTabCard.jsx
+++ src/component/pages/Tab/ReactTabCard/ReactTabCard.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
 
-const ReactTabCard = ({category}) => {
-    const {_id,Picture,Name,Price,Rating}=category
+const ReactTabCard = ({ category }) => {
+    const { _id, Picture, Name, Price, Rating } = category
+    const { user } = useContext(AuthContext)
+    const handleBuyNowClick = () => {
+        if (!user) {
+            Swal.fire('You have to login first!', 'Please Login!', 'error');
+        }
+    };
     return (
         <div>
-             <div className="card w-96 bg-base-100 shadow-xl ">
+            <div className="card w-96 bg-base-100 shadow-xl ">
                 <figure><img src={Picture} alt="Toy Picture" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Product Name: {Name}</h2>
                     <div className='flex space-x-2 sm:flex-row'>
-                    <p className='text-xl '>Price: ${Price}</p>
-                    <p>{Rating}</p>
+                        <p className='text-xl '>Price: ${Price}</p>
+                        <p>{Rating}</p>
                     </div>
                     <div className="card-actions justify-end">
-                       <Link to={`/category/${_id}`}><button className="btn btn-primary">Buy Now</button></Link>
+                        <Link to={`/category/${_id}`}><button onClick={handleBuyNowClick} className="btn btn-primary">Buy Now</button></Link>
                     </div>
                 </div>
             </div>
